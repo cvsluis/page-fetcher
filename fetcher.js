6bd013d314dfdbbfd672ee39589ec8694f517baf
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-14 12:31:29 -0700

    feat: add function that checks if file already exists

--- fetcher.js
+++ fetcher.js
@@ -1,19 +1,39 @@
 const fs = require('fs');
 const request = require('request');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const url = process.argv[2];
 const localPath = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if an error was received
-  }
-
-  fs.writeFile(localPath, body, err => {
-    if (err) {
-      console.error(err);
+if (fs.existsSync(localPath)) {
+  rl.question("File path already exists! Would you like to overide? Y or N  ", (answer) => {
+    if (answer === 'Y') {
+      makeRequest();
+    } else {
+      console.log("Try again with new file path.");
+      process.exit();
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
   });
-});
+}
+
+const makeRequest = () => {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if an error was received
+    }
+
+    fs.writeFile(localPath, body, err => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
+      process.exit();
+    });
+  });
+};
